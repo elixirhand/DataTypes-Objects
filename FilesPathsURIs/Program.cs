8c9f187e92356d607fb1aa4f536dc7e9d1510a86
ElixirHand <rudph1@hotmail.com>
2020-06-13 22:39:41 +0100

    Creating Paths to the file system

---
+++ FilesPathsURIs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace FilesPathsURIs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public string GetInputFilePathShort()
+        {
+            var drive = @"C:\";
+            var dir = @"temp\pspath\";
+            var file = "test.txt";
+
+            string fullPath = Path.Combine(drive, dir, file);
+            return fullPath;
+        }
+
+        public string GetInputFilePathLong()
+        {
+            var drive = @"C:\";
+            var dir = @"temp\pspath\";
+            var file = "test.txt";
+
+            var fullPath = drive;
+
+            fullPath += dir;
+
+            if (!dir.EndsWith(@"\"))
+            {
+                fullPath += @"\";
+            }
+
+            fullPath += file;
+
+                return fullPath;
+        }
+
+    }
+}
