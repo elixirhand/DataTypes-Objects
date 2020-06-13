9f83612cc683f0647e69dd60a24aa63253e7140d
ElixirHand <rudph1@hotmail.com>
2020-06-13 14:06:25 +0100

    Refernce Equality checks

---
+++ ClassLibrary.Tests/ObjectReferenceEqualsExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace ClassLibrary.Tests
+{
+    public class ObjectReferenceEqualsExample
+    {
+        [Fact]
+        public void ExampleWhereReferenceTypeUsesValueEqualitySematics()
+        {
+            Uri uri1 = new Uri("https://elixirhand.com/");
+            Uri uri2 = new Uri("https://elixirhand.com/");
+
+            var areEqual = uri1 == uri2;
+
+            bool isSameRefernce = object.ReferenceEquals(uri1, uri2);
+
+            uri2 = uri1;
+
+            isSameRefernce = object.ReferenceEquals(uri1, uri2);
+
+        }
+    }
+}
